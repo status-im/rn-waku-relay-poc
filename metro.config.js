cb8bfbe24a5f284003b87996dd4fa34512c9e201
jemboh <jeremy@status.im>
2022-04-08 12:55:15 +0100

    added dependencies and loading them in metro

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,15 @@
  * @format
  */
 
-module.exports = {
+ module.exports = {
+  resolver: {
+    extraNodeModules: {
+      assert: require.resolve('assert'),
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
